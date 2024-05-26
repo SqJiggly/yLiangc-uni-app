7cb12cef1f2ea076c7b4b267381a418d0d5b65cd
PYAN\p <2505091634@qq.com>
2024-05-26 15:12:54 +0800

    fix: 尝试唤起弹窗

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -3,6 +3,8 @@
 		<button @click="toDetail">去到详情</button>
 		<u-button type="primary" text="确定"></u-button>
 		<view>{{name}}111222</view>
+		<view class="test-rpx">test</view>
+		
 		<my-header></my-header>
 	</view>
 </template>
@@ -32,6 +34,10 @@
 	}
 </script>
 
-<style lang="scss">
-
+<style scoped lang="scss">
+.test-rpx{
+	width: 375rpx;
+	height: 260rpx;
+	background: red;
+}
 </style>
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<button open-type="getUserInfo" @getuserinfo="getuserinfo">登录</button>
+		<button @click.self="getUserProfile">登录</button>
 	</view>
 </template>
 
@@ -12,35 +12,49 @@
 			};
 		},
 		methods: {
-			getuserinfo(e){
-				let appid = 'wx68be33c1022aedcd'
-				const secret = '209c5f5562377bac4626bd9db94d0e94'
-				console.log(e, 'eeeeeeeee')
+			getUserProfile(){
+				console.log('6666666')
+				uni.getUserProfile({
+			      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+			      success: (res) => {
+					  console.log(res, "resssssssssss")
+			        // this.setData({
+			        //   userInfo: res.userInfo,
+			        //   hasUserInfo: true
+			        // })
+			      }
+			    })
+			},
+			
+		// 	getuserinfo(e){
+		// 		let appid = 'wx68be33c1022aedcd'
+		// 		const secret = '209c5f5562377bac4626bd9db94d0e94'
+		// 		console.log(e, 'eeeeeeeee')
 				
-				uni.login({
-					provider:'weixin'
-				}).then(res => {
-						const [,{errMsg,code}] = res
-						let url = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code';
-						    uni.request({
-						      url: url, // 请求路径
-						      method: 'GET', //请求方式
-						      success: result => {
-						        //响应成功
-						        //这里就获取到了openid了
-						        console.info(result.data.openid);
-						        uni.setStorage({
-						          key:'user',
-						          data: result.data.openid
-						        })
-						      },
-						      fail: err => {} //失败
+		// 		uni.login({
+		// 			provider:'weixin'
+		// 		}).then(res => {
+		// 				const [,{errMsg,code}] = res
+		// 				let url = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code';
+		// 				    uni.request({
+		// 				      url: url, // 请求路径
+		// 				      method: 'GET', //请求方式
+		// 				      success: result => {
+		// 				        //响应成功
+		// 				        //这里就获取到了openid了
+		// 				        console.info(result.data.openid);
+		// 				        uni.setStorage({
+		// 				          key:'user',
+		// 				          data: result.data.openid
+		// 				        })
+		// 				      },
+		// 				      fail: err => {} //失败
 						
 
-		})
+		// })
 		
-				})
-			}
+		// 		})
+		// 	}
 		},
 		onShow() {
 			
